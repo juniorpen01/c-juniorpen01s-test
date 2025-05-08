85c38afa258ed467da43800a9f5e1b4833cddf74
juniorpen01 <01juniorpen@gmail.com>
2025-05-08 18:01:47 +0800

    Implement _Generic

--- juniorpen01s_test/include/juniorpen01s_test.h
+++ juniorpen01s_test/include/juniorpen01s_test.h
@@ -7,12 +7,4 @@ typedef void (*Test)(void);
 
 VECTOR(Test)
 
-typedef struct Tests {
-  Vector_Test value;
-} Tests;
-
-Tests Tests_New(void) { return (Tests){Vector_Test_New()}; }
-
-static Tests tests = Tests_New();
-
 #endif
--- juniorpen01s_test/src/juniorpen01s_test.c
+++ juniorpen01s_test/src/juniorpen01s_test.c
@@ -1,1 +1,3 @@
 #include <juniorpen01s_test.h>
+
+// int main(void) { FOO(Tests_New()); }
